ae76057101b809b3032217995823ea0216c44ff1
KotikRomikov <snobo2@gmail.com>
2020-04-07 19:10:13 +0300

    сделал Мурманск репетиторы

--- app/src/main/java/com/example/infokids/murmansk/MurmanskHobbyActivity.kt
+++ app/src/main/java/com/example/infokids/murmansk/MurmanskHobbyActivity.kt
@@ -1,13 +1,33 @@
 package com.example.infokids.murmansk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.infokids.R
+import com.example.infokids.murmansk.hobby.MurmanskHobbyCiprActivity
+import com.example.infokids.murmansk.hobby.MurmanskHobbyDevelopmentCentreActivity
+import com.example.infokids.murmansk.hobby.MurmanskHobbyTutorsActivity
 
 class MurmanskHobbyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_murmansk_hobby)
+    }
+
+    fun murmansk_hobby_cipr(view: View) {
+        val murmanskHobbyCiprIntent = Intent(this,MurmanskHobbyCiprActivity::class.java)
+        startActivity(murmanskHobbyCiprIntent)
+    }
+
+    fun murmansk_hobby_development_centre(view: View) {
+        val murmanskHobbyDevelopmentCentreIntent = Intent(this, MurmanskHobbyDevelopmentCentreActivity::class.java)
+        startActivity(murmanskHobbyDevelopmentCentreIntent)
+    }
+
+    fun murmansk_hobby_tutors(view: View) {
+        val tutorsIntent = Intent(this,MurmanskHobbyTutorsActivity::class.java)
+        startActivity(tutorsIntent)
     }
 }
