65af4f5a5d348920f9b7d8ed70d6adcb897fe9ed
fatalist420 <fatalist420@gmail.com>
2020-04-15 23:17:54 +0300

    + Apatiti Hobby part

---
+++ app/src/main/java/com/example/infokids/apatiti/ApatitiHobbyActivity.kt
@@ -0,0 +1,74 @@
+package com.example.infokids.apatiti
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.infokids.R
+import com.example.infokids.apatiti.hobby.ApatitiHobbyCiprActivity
+import com.example.infokids.apatiti.hobby.ApatitiHobbyDevelopmentCentreActivity
+import com.example.infokids.apatiti.hobby.ApatitiHobbySportsSectionActivity
+import com.example.infokids.apatiti.hobby.ApatitiHobbyTutorsActivity
+import com.example.infokids.murmansk.hobby.*
+
+class ApatitiHobbyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apatiti_hobby)
+    }
+
+    fun apatiti_hobby_cipr(view: View) {
+        val apatitiHobbyCiprIntent = Intent(this, ApatitiHobbyCiprActivity::class.java)
+        startActivity(apatitiHobbyCiprIntent)
+    }
+
+    fun apatiti_hobby_development_centre(view: View) {
+        val apatitiHobbyDevelopmentCentreIntent = Intent(this, ApatitiHobbyDevelopmentCentreActivity::class.java)
+        startActivity(apatitiHobbyDevelopmentCentreIntent)
+    }
+
+    fun apatiti_hobby_tutors(view: View) {
+        val tutorsIntent = Intent(this, ApatitiHobbyTutorsActivity::class.java)
+        startActivity(tutorsIntent)
+    }
+
+    fun DecorativeApplied(view: View) {
+        val decorativeAppliedIntent = Intent(this,MurmanskHobbyDecorativeAppliedActivity::class.java)
+        startActivity(decorativeAppliedIntent)
+    }
+
+    fun technical(view: View) {
+        val technicalIntent = Intent(this,MurmanskHobbyTechActivity::class.java)
+        startActivity(technicalIntent)
+    }
+
+    fun hobby_choreography(view: View) {
+        val choreographyIntent = Intent(this,MurmanskHobbyChoreographyActivity::class.java)
+        startActivity(choreographyIntent)
+    }
+
+    fun MusicVocal(view: View) {
+        val musicVocalIntent = Intent(this,MurmanskHobbyMusicVocalActivity::class.java)
+        startActivity(musicVocalIntent)
+    }
+
+    fun Languages(view: View) {
+        val languageIntent = Intent(this,MurmanskHobbyLanguagesActivity::class.java)
+        startActivity(languageIntent)
+    }
+
+    fun artwork(view: View) {
+        val artWorkIntent = Intent(this,MurmanskHobbyArtWorkActivity::class.java)
+        startActivity(artWorkIntent)
+    }
+
+    fun centerOfDevelopment(view: View) {
+        val centerOfDevelopmentIntent = Intent(this,MurmanskHobbyCenterOfDevelopmentActivity::class.java)
+        startActivity(centerOfDevelopmentIntent)
+    }
+    fun SportsSection(view: View) {
+        val sportsSectionIntent = Intent(this, ApatitiHobbySportsSectionActivity::class.java)
+        startActivity(sportsSectionIntent)
+    }
+}
