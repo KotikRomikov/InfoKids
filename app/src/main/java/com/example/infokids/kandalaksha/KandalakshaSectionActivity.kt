1b2d553cbd20a058f828cb877eebfcdd7504ba5e
KotikRomikov <snobo2@gmail.com>
2020-04-15 23:39:17 +0300

    Кандалакша резвлечения

--- app/src/main/java/com/example/infokids/kandalaksha/KandalakshaSectionActivity.kt
+++ app/src/main/java/com/example/infokids/kandalaksha/KandalakshaSectionActivity.kt
@@ -1,5 +1,6 @@
 package com.example.infokids.kandalaksha
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -19,5 +20,8 @@ class KandalakshaSectionActivity : AppCompatActivity() {
     fun HobbySection(view: View) {}
     fun ShopSection(view: View) {}
     fun RelaxationSection(view: View) {}
-    fun MedicalSection(view: View) {}
+    fun MedicalSection(view: View) {
+        val MedicalSectionIntent = Intent(this,KandalakshaMedicalActivity::class.java)
+        startActivity(MedicalSectionIntent)
+    }
 }
---
+++ app/src/main/java/com/example/infokids/kandalaksha/relaxation/KandalakshaRelaxationSportActivity.kt
@@ -0,0 +1,13 @@
+package com.example.infokids.kandalaksha.relaxation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.infokids.R
+
+class KandalakshaRelaxationSportActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kandalaksha_relaxation_sport)
+    }
+}
