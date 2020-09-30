41e9d01e58945919e55d8055d76a8e81d1f08e22
Fatalist420 <fatalist420@gmail.com>
2020-09-30 19:20:00 +0300

    + путь самурая, поправил поменял иконку

--- app/src/main/java/com/dprk/infokids51/MenuActivity.kt
+++ app/src/main/java/com/dprk/infokids51/MenuActivity.kt
@@ -1,11 +1,8 @@
 package com.dprk.infokids51
 
-import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MenuActivity : AppCompatActivity() {
